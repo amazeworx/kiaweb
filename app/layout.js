4db9a5b739d213ad0f8fef52bc13b492e5f5f329
Slant Dev <build@amazeworx.com>
2024-12-17 00:03:55 +0700

    change favicon

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import Head from "next/head";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
       </body>
