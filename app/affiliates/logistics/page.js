a138d9eb97126d8ea9e0fcc68a4aa4442ae359a5
Slant Dev <build@amazeworx.com>
2024-12-17 02:25:54 +0700

    revision

--- app/affiliates/logistics/page.js
+++ app/affiliates/logistics/page.js
@@ -12,7 +12,7 @@ export default function AboutUsPage() {
       <Header backgroundColor="#f4f4f4" />
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row">
-          <div className="w-full lg:w-1/3 xl:w-1/4 order-2 lg:order-1">
+          <div className="w-full lg:w-1/3 xl:w-1/4 2xl: order-2 lg:order-1">
             <div className="px-4 py-6 lg:pl-6 lg:pr-10 lg:py-10">
               <h1 className="text-4xl lg:text-[64px] leading-[1.1] mb-4 xl:pt-20">
                 Logistics / Mining / Trading / Ports
@@ -37,7 +37,7 @@ export default function AboutUsPage() {
           </div>
         </div>
       </div>
-      <div className="hidden lg:block absolute top-0 right-0 h-screen lg:w-2/3 xl:w-3/4">
+      <div className="hidden lg:block absolute top-0 right-0 h-screen lg:w-2/3 xl:w-3/4 2xl:w-2/3">
         <Image
           src="/images/logistics-03.jpg"
           alt="Logistics"
